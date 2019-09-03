233d97ab36343f879c87ab31a37c98136b28bb29
Evandro Gardolin <evandrogardolinn@gmail.com>
2019-09-03 20:26:44 -0300

    [FIX] routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -10,4 +10,32 @@ Route::post('articles', 'ArticleController@store');
 Route::put('articles/{article}', 'ArticleController@update');
 Route::delete('articles/{article}', 'ArticleController@delete');
 
+Route::get('autor', 'AutorController@index');
+Route::get('autor/{article}', 'AutorController@show');
+Route::post('autor', 'AutorController@store');
+Route::put('autor/{article}', 'AutorController@update');
+Route::delete('autor/{article}', 'AutorController@delete');
 
+Route::get('livros', 'LivroController@index');
+Route::get('livros/{livros}', 'LivroController@show');
+Route::post('livros', 'LivroController@store');
+Route::put('livros/{livros}', 'LivroController@update');
+Route::delete('livros/{livros}', 'LivroController@delete');
+
+Route::get('autor-livro', 'AutorLivroController@index');
+Route::get('autor-livro/{autorlivro}', 'AutorLivroController@show');
+Route::post('autor-livro', 'AutorLivroController@store');
+Route::put('autor-livro/{autorlivro}', 'AutorLivroController@update');
+Route::delete('autor-livro/{autorlivro}', 'AutorLivroController@delete');
+
+Route::get('usuarios', 'UsuariosDaLivrariaController@index');
+Route::get('usuarios/{usuarios}', 'UsuariosDaLivrariaController@show');
+Route::post('usuarios', 'UsuariosDaLivrariaController@store');
+Route::put('usuarios/{usuarios}', 'UsuariosDaLivrariaController@update');
+Route::delete('usuarios/{usuarios}', 'UsuariosDaLivrariaController@delete');
+
+Route::get('locacoes', 'LocacoesController@index');
+Route::get('locacoes/{locacoes}', 'LocacoesController@show');
+Route::post('locacoes', 'LocacoesController@store');
+Route::put('locacoes/{locacoes}', 'LocacoesController@update');
+Route::delete('locacoes/{locacoes}', 'LocacoesController@delete');
